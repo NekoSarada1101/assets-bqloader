6a1957a31741b393f80a184dfea2d87f35463071
NekoSarada1101 <ryoyukikazu11@gmail.com>
2023-01-26 14:57:57 +0900

    ログと例外処理を追加

--- main.py
+++ main.py
@@ -1,62 +1,90 @@
+import sys
 import json
+import logging
 
 import pandas as pd
 import requests
 from google.cloud import bigquery, storage, secretmanager
 
 
-gsc_service_account_info = json.load(open('cloud_storage_credentials.json'))
-gcs_credentials = service_account.Credentials.from_service_account_info(gsc_service_account_info)
-gcs_client = storage.Client(
-    credentials=gcs_credentials,
-    project=gcs_credentials.project_id,
-)
+# constant ============================================
+bq_client = bigquery.Client()
+gcs_client = storage.Client()
+secret_client = secretmanager.SecretManagerServiceClient()
 
 SLACK_WEBHOOK_URL = secret_client.access_secret_version(request={'name': 'projects/831232013080/secrets/SECRETARY_BOT_V2_SLACK_WEBHOOK_GCP_NOTICE_URL/versions/latest'}).payload.data.decode('UTF-8')
 
 
+# logger ===============================================
+class JsonFormatter(logging.Formatter):
+    def format(self, log):
+        return json.dumps({
+            'level': log.levelname,
+            'message': log.getMessage(),
+        })
 
+
+formatter = JsonFormatter()
+stream = logging.StreamHandler(stream=sys.stdout)
+stream.setFormatter(formatter)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(stream)
+
+
+# functions ==========================================
 def bqloader(event, context):
-    print('Event ID: {}'.format(context.event_id))
-    print('Event type: {}'.format(context.event_type))
-    print('Bucket: {}'.format(event['bucket']))
-    print('File: {}'.format(event['name']))
-    print('Metageneration: {}'.format(event['metageneration']))
-    print('Created: {}'.format(event['timeCreated']))
-    print('Updated: {}'.format(event['updated']))
+    logger.info('===== START assets bqloader =====')
+    logger.info('Event ID={}'.format(context.event_id))
+    logger.info('Event type={}'.format(context.event_type))
+    logger.info('Bucket={}'.format(event['bucket']))
+    logger.info('File={}'.format(event['name']))
+    logger.info('Metageneration={}'.format(event['metageneration']))
+    logger.info('Created={}'.format(event['timeCreated']))
+    logger.info('Updated={}'.format(event['updated']))
 
-    # GCSに配置されたファイルをダウンロード
-    bucket = gcs_client.get_bucket(event['bucket'])
-    blob = bucket.blob(event['name'])
-    blob.download_to_filename('/tmp/' + event['name'])
+    try:
+        # GCSに配置されたファイルをダウンロード
+        logger.info('----- download gcs asset file -----')
+        bucket = gcs_client.get_bucket(event['bucket'])
+        blob = bucket.blob(event['name'])
+        blob.download_to_filename('/tmp/' + event['name'])
 
-    # 列名を変更
-    df = pd.read_csv('/tmp/' + event['name'], header=0, encoding='shift-jis')
-    df.rename(columns={'日付': 'date', '合計（円）': 'total', '預金・現金・暗号資産（円）': 'cash', '投資信託（円）': 'investment_trust', 'ポイント（円）': 'point'}, inplace=True)
+        # 列名を変更
+        logger.info('----- rename row name -----')
+        df = pd.read_csv('/tmp/' + event['name'], header=0, encoding='shift-jis')
+        df.rename(columns={'日付': 'date', '合計（円）': 'total', '預金・現金・暗号資産（円）': 'cash', '投資信託（円）': 'investment_trust', 'ポイント（円）': 'point'}, inplace=True)
 
-    # 日付をyyyy-mm-ddに変更
-    df['date'] = df['date'].str.replace('/', '-')
+        # 日付をyyyy-mm-ddに変更
+        df['date'] = df['date'].str.replace('/', '-')
 
-    # NaNを含む行を削除する
-    df = df.dropna(how='any')
-    print(df)
+        # NaNを含む行を削除する
+        df = df.dropna(how='any')
+        logger.debug('dataframe={}'.format(df))
 
-    # BQにインサート
-    table = bq_client.get_table('slackbot-288310.my_dataset.asset_trend')
-    result = bq_client.insert_rows_from_dataframe(table, df)
-    print(result)
+        # BQにインサート
+        logger.info('----- insert bq -----')
+        table = bq_client.get_table('slackbot-288310.my_dataset.asset_trend')
+        result = bq_client.insert_rows_from_dataframe(table, df)
 
-    data = {
-        "attachments": [
-            {
-                "color": "ffffff",
-                "text": '資産推移をBQに登録しました。',
-            }
-        ]
-    }
-    print(data)
+        logger.info('----- post slack chat message -----')
+        data = {
+            "attachments": [
+                {
+                    "color": "ffffff",
+                    "text": '資産推移をBQに登録しました。',
+                }
+            ]
+        }
+        logger.info('payload={}'.format(data))
 
-    json_data = json.dumps(data).encode("utf-8")
-    response = requests.post(SLACK_INCOMING_WEBHOOK_URL, json_data)
-    print(response)
-    print(response.text)
+        json_data = json.dumps(data).encode("utf-8")
+        response = requests.post(SLACK_WEBHOOK_URL, json_data)
+        logger.info('response={}'.format(response))
+        logger.info('response text={}'.format(response.text))
+
+    except Exception as e:
+        logger.error(e)
+    finally:
+        logger.info('===== END assets bqloader =====')
